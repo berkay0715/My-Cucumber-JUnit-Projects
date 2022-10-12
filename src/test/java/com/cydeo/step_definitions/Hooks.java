795f81553456e07fc91101eaf101071d496f7215
Berkay <celib181_aehit@students.vizja.pl>
2022-10-12 22:54:12 +0200

    Cucumber

---
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -0,0 +1,43 @@
+package com.cydeo.step_definitions;
+
+/*
+In this class we will be able to pass pre & post - conditions to
+each scenario and each step
+ */
+
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
+
+public class Hooks {
+
+    //import from io.cucumber.java not from JUnit
+    @Before(order = 1)
+    public void setupScenario(){
+
+        System.out.println("====Setting up browser using cucumber @Before");
+    }
+    @Before(value = "@login",order = 2)
+    public void setUpScenarioForLoginS(){
+        System.out.println("====This will only apply to scenarios with @login tag");
+    }
+    @Before(value = "@db",order = 0)
+    public void setUpForDatabaseScenarios() {
+        System.out.println("====This will only apply to scenarios with @db tag");
+    }
+    @After
+    public void TeardownScenario(){
+        System.out.println("====Closing browser using cucumber @After");
+        System.out.println("==== Scenario ended / Take screenshot if failed!");
+    }
+    @BeforeStep
+    public void setupStep(){
+        System.out.println("-------> applying setup using @BeforeStep");
+    }
+    @AfterStep
+    public void afterSetup(){
+        System.out.println("-------> applying teardown using @AfterStep");
+    }
+
+}
---
+++ src/test/java/com/cydeo/step_definitions/login_StepDefinitions.java
@@ -0,0 +1,49 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class login_StepDefinitions {
+
+
+    @When("user enters librarian username")
+    public void user_enters_librarian_username() {
+
+        System.out.println("User should enter librarian username");
+    }
+    @When("user enters librarian password")
+    public void user_enters_librarian_password() {
+        System.out.println("User should enter librarian password");
+    }
+    @Then("user should see the dashboard")
+    public void user_should_see_the_dashboard() {
+        System.out.println("user should see the dashboard");
+
+    }
+    @When("user enters student username")
+    public void user_enters_student_username() {
+        System.out.println("user should enter student username");
+    }
+    @And("user enters student password")
+    public void user_enters_student_password() {
+        System.out.println("user should enter student password");
+    }
+    @When("user enters admin username")
+    public void user_enters_admin_username() {
+        System.out.println("user should enter admin username");
+    }
+    @And("user enters admin password")
+    public void user_enters_admin_password() {
+        System.out.println("user should enter admin password ");
+    }
+
+    @Given("user is on library login page")
+    public void user_is_on_library_login_page() {
+        System.out.println("user is on the library login page");
+    }
+
+
+
+}
