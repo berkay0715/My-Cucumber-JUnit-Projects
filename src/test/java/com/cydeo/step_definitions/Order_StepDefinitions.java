0c5de258f22583e59abbd64983dfdaf529566891
Berkay <celib181_aehit@students.vizja.pl>
2022-10-19 17:52:46 +0200

    Java

--- src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
@@ -1,21 +1,32 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.BasePage;
 import com.cydeo.pages.WebTableLoginPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.support.ui.Select;
 
 public class webTableOrder_StepDefinitions {
 
     WebTableLoginPage webTableLoginPage = new WebTableLoginPage();
+
+    BasePage basePage = new BasePage();
     @Given("user is already logged in and on order page")
     public void user_is_already_logged_in_and_on_order_page() {
-       webTableLoginPage.login();
+
+        //Calling out login method to log in to web table app
+        webTableLoginPage.login();
+
+        //clicking to "order" link to go order page
+       basePage.order.click();
+
 
     }
     @When("user selects product type {string}")
     public void user_selects_product_type(String string) {
+
 
     }
     @When("user enters quantity {int}")
