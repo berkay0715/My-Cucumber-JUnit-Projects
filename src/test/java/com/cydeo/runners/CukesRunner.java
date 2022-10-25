0ec029aa91744dbd189bcb5c3d1143a8869d2cc9
Berkay <celib181_aehit@students.vizja.pl>
2022-10-25 19:04:44 +0200

    JUnit

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -7,14 +7,16 @@ import org.junit.runner.RunWith;
     @RunWith(Cucumber.class)
     @CucumberOptions(
             plugin = {
+                    "pretty",
                     "html:target/cucumber-reports.html",
                     "rerun:target/rerun.txt",
-                    "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                    "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                    "json:target/cucumber.json",
             },
             features = "src/test/resources/features",
             glue = "com/cydeo/step_definitions",
             dryRun = false,
-            tags = "@smoke"
+            tags = ""
     )
     public class CukesRunner {
     }
